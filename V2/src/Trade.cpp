63c7a6475b01c8ec4562581a2dd55f54c9416495
navihx <932942142@qq.com>
2021-05-26 21:14:11 +0800

    增加余额 判断负数

--- V2/src/Trade.cpp
+++ V2/src/Trade.cpp
@@ -694,6 +694,8 @@ bool Trade::setbal(const std::string &uname, double b)
 
 bool Trade::addbal(const std::string &uname, double b)
 {
+    if (b < 0)
+        return false;
     for (auto uit : userList)
     {
         if (uname.compare(uit->getName()) == 0)
@@ -713,7 +715,7 @@ bool Trade::redbal(const std::string &uname, double b)
         if (uname.compare(uit->getName()) == 0)
         {
             // uit->setBalance(b - uit->getBalance());
-            return  uit->redBalance(b);
+            return uit->redBalance(b);
         }
     }
     return false;
