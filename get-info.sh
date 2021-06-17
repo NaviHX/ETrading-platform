7a91dcb84b98b7120c7255c29c7b9fb6266e5371
navihx <932942142@qq.com>
2021-06-17 09:00:03 +0800

    展示文件md5

--- get-info.sh
+++ get-info.sh
@@ -2,7 +2,11 @@ cat commdata.txt | \
 awk 'BEGIN {print("COMMODITY Info:")} \
 !- /^[0123456789]/ {printf("ID: %s\tType: %s\tPrice: %s\tQuant: %s\tOwn: %s\tDesc: %s\tDisc: %s\n",$1,$2,$4,$6,$3,$7,$5)} \
 - /^[0123456789]/ {printf("Discount:\nBook: %s\tFood: %s\tCloth: %s\n",$1,$2,$3)}'
+cat commmd5.txt | \
+awk '{printf("commdata md5 checksum : %s\n",$1)}'
 
 cat userdata.txt | \
 awk 'BEGIN {print("USER Info:")} \
 {printf("ID: %s\tPWD: %s\tBalance: %s\tType: %s\n",$1,$2,$4,$3)}'
+cat usermd5.txt | \
+awk '{printf("userdata md5 checksum : %s\n",$1)}'
